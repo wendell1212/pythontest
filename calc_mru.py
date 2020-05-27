8f6284cefb9b224a80a835fe82d898c54b09427f
user1212 <62248847+wendell1212@users.noreply.github.com>
2020-05-27 11:43:20 -0300

    Create calc_mru.py

---
+++ calc_mru.py
@@ -0,0 +1,7 @@
+s_inicial = float(input())
+v_movel = float(input())
+t_total = float(input())
+
+s_final = s_inicial + v_movel * t_total
+print('Posição final do móvel')
+print(f'S({t_total:.1f}) = {s_final:.1f} m')
